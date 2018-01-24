f9a441e37ff40619b16b03b11e672d14ec8640ef
unknown <sayandutta37@gmail.com>
2018-01-24 17:56:20 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import SingleTodo from './SingleTodo';
+
+class App extends Component {
+  constructor(){
+    super();
+    this.state={
+      todos:[],
+      currentTodo:""
+    };
+  }
+  onClick=()=>{
+    let todosCopy=this.state.todos.slice();
+    todosCopy.push(this.state.currentTodo);
+
+    this.setState({todos:todosCopy,currentTodo:""});
+  }
+  onInputChange=e=>{
+    this.setState({currentTodo:e.target.value})
+  }
+  deleteTodo = i=>{
+     let todosCopy=this.state.todos.slice();
+     todosCopy.splice(i,1);
+     this.setState({todos:todosCopy});
+  }
+  render(){
+    let bulletedTodos=this.state.todos.map((e,i)=>{
+      return(
+        <SingleTodo todo={e} delete={()=>this.deleteTodo(i)} />
+      );
+    });
+    return(
+      <div>
+        <input placeholder="Enter Todo" value={this.state.currentTodo}
+        onChange={this.onInputChange}/>
+        <button onClick={this.onClick}>Add!</button>
+        <br />
+        {this.state.todos.length===0? "You dont have todos":<ul>{bulletedTodos}</ul>}
+      </div>
+    )
+  }
+}
+
+export default App;
